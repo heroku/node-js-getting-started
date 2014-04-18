9717c83d100192a6f9540ba1a03e1bde44ee00c6
zeke <zeke@sikelianos.com>
2014-04-18 14:27:10 -0700

    express 4!

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var app = express();
-app.use(express.logger());
 
+app.set('port', (process.env.PORT || 5000))
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
 });
