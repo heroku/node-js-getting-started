b3b8463c148491f1064e035cef4530c52ad9ef64
Hiroshi Nagata <hirosn@live.com>
2014-11-02 01:28:34 -0700

    Demo

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-var express = require('express')
+var express = require('express');
+var cool = require('cool-ascii-faces');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+  response.send(cool());
+});
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log('Node app is running at localhost:', app.get('port'));
+});
