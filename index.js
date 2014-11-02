d90b1211b49b0cf204ac43400b0f79fffec872c6
Hiroshi Nagata <hirosn@live.com>
2014-11-02 01:41:40 -0700

    Add config vars.

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send(cool());
+  var result = '';
+  var times = process.env.TIMES || 5;
+
+  for (i = 0; i < times; i++) {
+    result += cool();
+  }
+
+  response.send(result);
 });
 
 app.listen(app.get('port'), function() {
