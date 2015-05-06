0de9cabb19d54d26c816949a1ec636ee60d770ff
typham <phamty113@gmail.com>
2015-05-06 15:45:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 80));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
