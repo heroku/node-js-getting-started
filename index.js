d6cb9736a04424ae1db24f478889de2216eb2701
jeremielenoir <lenoir.jeremie@roodeo.com>
2015-08-24 19:56:09 +0200

    test

--- index.js
+++ index.js
@@ -1175,6 +1175,11 @@ app.use(function(req, res, next) {
 //
 app.use('/api', router);
 app.use('/', publicRouter);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 // START THE SERVER
