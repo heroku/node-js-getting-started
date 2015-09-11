aeee523d69dc259002abd0e40612f4e129fab221
System Administrator <root@MacBook-Pro-de-Macbook.local>
2015-09-11 12:26:54 +0200

    add params to activate or not auth

--- index.js
+++ index.js
@@ -1169,7 +1169,7 @@ publicRouter.get('/number/:number', function(req, res, next) {
             , function(stdoutFinal){
               console.log('IMG FINAL', imgFinal);
 
-            var imgFinalMozaic = im(imgDestPath + '/' + number + '-temp-3.jpg');
+            var imgFinalMozaic = im(imgDestPath + '/' + number + '-temp-final.jpg');
             imgFinalMozaic.stream(function(err, stdout, stderr) {
 
               var buf = new Buffer('');
