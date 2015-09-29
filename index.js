671eb6e77d0158e6982775aceebd06f79b606231
System Administrator <root@MacBook-Pro-de-Macbook.local>
2015-09-29 13:51:36 +0200

    test drawing

--- index.js
+++ index.js
@@ -1194,18 +1194,23 @@ var createMozaic = function(number, tempFaces, callback){
 
   //console.log('TEMPFACES', tempFaces);
   var im = gm;//.subClass({ imageMagick: true });
-  im(450, 450, "#000").drawText(10, 50, "from scratch").write(imgDestPath + '/' + number + '-test.png', function (err2) {
-    console.log('TEST IMAGE', err2);
+  im(450, 450, "#000").drawText(10, 50, "from scratch").write(imgDestPath + '/' + number + '-temp1.png', function (err1) {
     gm()
     .command("composite")
     .in("-page", "+0+0")
-    .in(publicPath + tempFaces[3].picture)
-    .in(imgDestPath + '/' + number + '-test.png')
-    .write(imgDestPath + '/' + number + '-test2.png' , function (err) {
-      if (!err)
-        console.log(' hooray! ');
-      else
-        console.log(err);
+    .in(publicPath + tempFaces[0].picture)
+    .in(imgDestPath + '/' + number + '-temp1.png')
+    .write(imgDestPath + '/' + number + '-temp2.png' , function (err2) {
+      console.log('TEST IMAGE', err2);
+      gm()
+      .command("composite")
+      .in("-page", "+150+0")
+      .in(publicPath + tempFaces[1].picture)
+      .in(imgDestPath + '/' + number + '-temp2.png')
+      .write(imgDestPath + '/' + number + '-temp3.png' , function (err3) {
+        console.log('TEST IMAGE', err3);
+
+      });
     });
   });
 
