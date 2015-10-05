53edce356df231d4bc50568d699b91a9b0000147
System Administrator <root@MacBook-Pro-de-Macbook.local>
2015-10-05 14:36:30 +0200

    change database server

--- index.js
+++ index.js
@@ -515,10 +515,11 @@ router.route('/faces')
                 var range = JSON.parse('[' + req.params.range + ']');
                 mongoose.connection.close(function () {
                   console.log('Mongoose connection disconnected');
+                  mongoose.connect(config.mongodb,{}, function(err){
+                    console.log(err);
+                  });
                 });
-                mongoose.connect(config.mongodb,{}, function(err){
-                  console.log(err);
-                });
+
                 Face.find({number:{$in:range}}).sort('number').exec(function(err, faces) {
 
                     var tempFaces = _.clone(faces);
