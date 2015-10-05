b8aa65a1c37f07963a0375012ff45d1701208602
System Administrator <root@MacBook-Pro-de-Macbook.local>
2015-10-05 14:11:04 +0200

    change database server

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ var mongoose   = require('mongoose');
 var config   = require('./app/config');
 mongoose.connect(config.mongodb); // connect to our database
 
-
-
 var Face     = require('./app/models/face');
 var Scrap     = require('./app/models/scrap');
 
@@ -512,7 +510,8 @@ router.route('/faces')
 
             // get the face with that id (accessed at GET http://localhost:8080/api/faces/:face_id)
             .get(function(req, res) {
-                mongoose.connect(config.mongodb,{}, function(){
+                mongoose.connect(config.mongodb,{}, function(err){
+                  if (err) { throw err; }
                   console.log('MONGOOSE', mongoose.connection.readyState);
                 });
 
