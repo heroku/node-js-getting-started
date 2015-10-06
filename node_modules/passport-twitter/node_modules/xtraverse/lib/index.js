7ba6d96548eea2475f9e471e70444cdb7ffdc841
Sylvain Savajols <s.savajols@gmail.com>
2015-10-06 07:33:19 +0200

    remove node_modules cache

--- node_modules/passport-twitter/node_modules/xtraverse/lib/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Module dependencies.
- */
-var Collection = require('./collection');
-
-/**
- * Expose `wrap` as package function.
- */
-module.exports = Collection.wrap;
--- node_modules/paypal-rest-sdk/index.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('./lib/paypal-rest-sdk.js')();
