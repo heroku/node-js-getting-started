d5dc4493c420b8aa43bdd0f10e400b883fd1d40c
System Administrator <root@MacBook-Pro-de-Macbook.local>
2015-10-08 13:54:26 +0200

    silouhettes

--- index.js
+++ index.js
@@ -1224,7 +1224,7 @@ var getImagesForMozaic = function(number, callback){
         for(var i = 0; i < numberArray.length; i++){
 
           if( ! _.find(tempFaces, function(currentFace){ return currentFace.number == numberArray[i]; }) ){
-              var tempPicture = '/img/FREESTATE' + getRandomInt(1,15) + '.png';
+              var tempPicture = '/img/FREESTATE' + getRandomInt(1,19) + '.png';
               tempFaces.push({'number': numberArray[i], picture: tempPicture, downloaded: true});
               nbDownloads++;
           }
