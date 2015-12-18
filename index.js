d02661f64f1dc2cea5796ae12e0adc6c655e49cd
Tý Phạm <phamty113@gmail.com>
2015-12-18 17:31:58 +0700

    dfsdfsd

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ var app = express();
 app.set('port', (process.env.PORT || 80));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/about', function(request, response) {
+  response.send('About');
+});
+
+app.get('/contact', function(request, response) {
+  response.send('Contact');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-app.post('/*', function(req, res){
+app.post('/', function(req, res){
 
   res.sendfile('public/index.html');
 });
