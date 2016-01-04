c35bbda353e10ae241f70871b2eb0d56eda53875
Tania <taniapapazaf@gmail.com>
2016-01-04 17:04:06 +0000

    When scrolling, hash and top nav highlight get updated

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,19 @@
 (function () {
 	$( document ).ready(function() {
+        $(document).bind('scroll',function(e){
+            $('section').each(function(){
+                if (
+                $(this).offset().top < window.pageYOffset + 10
+                //begins before top
+                && $(this).offset().top + $(this).height() > window.pageYOffset + 10
+                //but ends in visible area
+                //+ 10 allows you to change hash before it hits the top border
+                ) {
+                    window.location.hash = $(this).attr('id');
+                    activateSection ( $(this).attr('id') );
+                }
+            });
+        });
 		
 		$('nav').on('click', 'li a',  function(){
 			var section = $(this).parent().attr('class');
