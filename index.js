7a562d492903cb07db17a29f0e32c1d984dd8deb
Hamza <ismnoiet@users.noreply.github.com>
2016-02-08 11:55:36 +0100

    Display the url in the terminal
    Make it easy for users to browse directly to their Express.js app by clicking on the url displayed in the terminal.Without having to go to the browser and type it manually.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/', function(request, response) {
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  console.log('Node app is running on port', app.get('port') + '  http://localhost:' + app.get('port'));  
 });
 
 
