ec886845168fe38ecaa4e7cdd434dc60ca172a01
Ravi Manchi <ravi.program@gmail.com>
2016-04-24 08:34:35 +1000

    fix bot

--- index.js
+++ index.js
@@ -21,7 +21,7 @@
 const http = require('http')
 const express = require('express')
 const bodyParser = require('body-parser')
-const Bot = require('./bot')
+const Bot = require('bot')
 
 
 let bot = new Bot({
