49afe46113288929f29c536ae4f80f9d89305d57
Yan Ma <yma@wayfair.com>
2016-07-08 20:22:48 -0400

    first commit. add the webhook response

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/webhook', function(req, res) {
+  res.send('Hello!!');
+  // if (req.query['hub.mode'] === 'subscribe' &&
+//       req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+//     console.log("Validating webhook");
+//     res.status(200).send(req.query['hub.challenge']);
+//   } else {
+//     console.error("Failed validation. Make sure the validation tokens match.");
+//     res.sendStatus(403);          
+//   }  
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
