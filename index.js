dc997cc5c3a3b6ae09521036f3a980986d49f2e8
David Blood <davidbjourno@users.noreply.github.com>
2016-08-23 17:41:53 +0100

    Demo

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
@@ -13,8 +14,10 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
