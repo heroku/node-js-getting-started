3c4674ff1c1f440d6af251d297caeea9ff77216e
sijocyber <sijocyber@gmail.com>
2016-10-14 19:21:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var express = require('express');
+/*var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -11,6 +11,30 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+*/
+var cool = require('cool-ascii-faces');
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index')
+});
+
+app.get('/cool', function(request, response) {
+  response.send(cool());
 });
 
 app.listen(app.get('port'), function() {
