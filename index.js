cb68faa2d4bbfe3762d4ae443555932b530c2eab
Medina Ali <medinaa@andrew.cmu.edu>
2016-11-03 12:42:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 var express = require('express');
 var app = express();
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
 
 app.set('port', (process.env.PORT || 5000));
 
