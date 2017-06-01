c40c532fdd8be5d6788b6fe87c73705c61b363a1
Kenneth Reitz <me@kennethreitz.org>
2017-06-01 13:52:38 -0400

    improve logging

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/', function(request, response) {
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  console.log(`Node app is running on http://0.0.0.0:${app.get('port')}`);
 });
 
 
