e37aec840dc791e308e1faa32297b61ab2452d5c
Jeremy Morrell <jmorrell@heroku.com>
2017-07-26 12:52:15 -0700

    This should fix it

--- test.js
+++ test.js
@@ -4,22 +4,25 @@ const test = require('tape');
 
 // Start the app
 const env = Object.assign({}, process.env, {PORT: 5000});
-const child = spawn('npm', ['start'], {env});
+const child = spawn('node', ['index.js'], {env});
 
 test('responds to requests', (t) => {
   t.plan(4);
 
-  // Make a request to our app
-  request('http://localhost:5000', (error, response, body) => {
-    // No error
-    t.false(error);
-    // Successful response
-    t.equal(response.statusCode, 200);
-    // Assert content checks
-    t.notEqual(body.indexOf("<title>Node.js Getting Started on Heroku</title>"), -1);
-    t.notEqual(body.indexOf("Getting Started with Node on Heroku"), -1);
+  // Wait until the server is ready
+  child.stdout.on('data', _ => {
+    // Make a request to our app
+    request('http://127.0.0.1:5000', (error, response, body) => {
+      // stop the server
+      child.kill();
 
-    // stop the server
-    child.kill();
+      // No error
+      t.false(error);
+      // Successful response
+      t.equal(response.statusCode, 200);
+      // Assert content checks
+      t.notEqual(body.indexOf("<title>Node.js Getting Started on Heroku</title>"), -1);
+      t.notEqual(body.indexOf("Getting Started with Node on Heroku"), -1);
+    });
   });
 });
