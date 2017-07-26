8fe3d60d4c3e9b6b0efe7898dbb15602efcce196
Jeremy Morrell <jmorrell@heroku.com>
2017-07-26 12:26:50 -0700

    Add a timeout

--- test.js
+++ test.js
@@ -9,17 +9,19 @@ const child = spawn('npm', ['start'], {env});
 test('responds to requests', (t) => {
   t.plan(4);
 
-  // Make a request to our app
-  request('http://localhost:5000', (error, response, body) => {
-    // No error
-    t.false(error);
-    // Successful response
-    t.equal(response.statusCode, 200);
-    // Assert content checks
-    t.notEqual(body.indexOf("<title>Node.js Getting Started on Heroku</title>"), -1);
-    t.notEqual(body.indexOf("Getting Started with Node on Heroku"), -1);
+  setTimeout(() => {
+    // Make a request to our app
+    request('http://localhost:5000', (error, response, body) => {
+      // No error
+      t.false(error);
+      // Successful response
+      t.equal(response.statusCode, 200);
+      // Assert content checks
+      t.notEqual(body.indexOf("<title>Node.js Getting Started on Heroku</title>"), -1);
+      t.notEqual(body.indexOf("Getting Started with Node on Heroku"), -1);
 
-    // stop the server
-    child.kill();
-  });
+      // stop the server
+      child.kill();
+    });
+  }, 100);
 });
