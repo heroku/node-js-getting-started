0ad170ee5baa36916f3060d3234bd1b61fc68624
Thibault Bedrignans <thib.bedrignans@gmail.com>
2017-11-08 14:29:03 +0100

    Add index.js to models folder

---
+++ app/models/index.js
@@ -0,0 +1,5 @@
+var Face            = require('./face');
+var Scrap           = require('./scrap');
+var Stat            = require('./stat');
+
+module.exports = {Face : Face, Scrap : Scrap, Stat : Stat};
