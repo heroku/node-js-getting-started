89a0ce9d9bcf8c76036b5fe5be1da71118822f97
= <thib.bedrignans@gmail.com>
2017-11-09 13:55:45 +0100

    Change router.Method() path on auth.js file

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -3,13 +3,13 @@ var router = express.Router();
 
 var passport = require('../passport');
 
-router.get('/auth/facebook/register/:id',function(req,res,next) {
+router.get('/facebook/register/:id',function(req,res,next) {
     passport.authenticate('facebook', {
         callbackURL: '/auth/facebook/callback/'+req.params.id 
     })(req,res,next);
 });
 
-router.get('/auth/facebook/callback/:id', function(req,res,next) {
+router.get('/facebook/callback/:id', function(req,res,next) {
     passport.authenticate('facebook',{
         callbackURL:"/auth/facebook/callback/" + req.params.id,
         successRedirect:"/success/" + req.params.id,
@@ -19,13 +19,13 @@ router.get('/auth/facebook/callback/:id', function(req,res,next) {
     }) (req,res,next);
 });
 
-router.get('/auth/twitter/register/:id', function(req,res,next) {
+router.get('/twitter/register/:id', function(req,res,next) {
     passport.authenticate('twitter', {
       callbackURL: '/auth/twitter/callback/'+req.params.id 
     })(req,res,next);
 });
 
-router.get('/auth/twitter/callback/:id', function(req,res,next) {
+router.get('/twitter/callback/:id', function(req,res,next) {
     passport.authenticate('twitter',{
         callbackURL:"/auth/twitter/callback/" + req.params.id,
         successRedirect:"/success/" + req.params.id,
@@ -34,7 +34,7 @@ router.get('/auth/twitter/callback/:id', function(req,res,next) {
     }) (req,res,next);
 });
 
-router.get('/auth/twitter/claim/:id',
+router.get('/twitter/claim/:id',
   function(req,res,next) {
     passport.authenticate('twitter',{
         callbackURL: '/auth/twitter/claim/callback/'+req.params.id 
@@ -42,14 +42,14 @@ router.get('/auth/twitter/claim/:id',
     )(req,res,next);
 });
 
-router.get('/auth/twitter/decline/:id',function(req,res,next) {
+router.get('/twitter/decline/:id',function(req,res,next) {
     passport.authenticate('twitter',{
         callbackURL: '/auth/twitter/decline/callback/'+req.params.id
     }
     )(req,res,next);
 });
 
-router.get('/auth/twitter/decline/callback/:id', function(req,res,next) {
+router.get('/twitter/decline/callback/:id', function(req,res,next) {
     passport.authenticate('twitter',{
         callbackURL:"/auth/twitter/decline/callback/" + req.params.id,
         successRedirect:"/decline/" + req.params.id,
@@ -59,7 +59,7 @@ router.get('/auth/twitter/decline/callback/:id', function(req,res,next) {
 });
 
 
-router.get('/auth/twitter/claim/callback/:id', function(req,res,next) {
+router.get('/twitter/claim/callback/:id', function(req,res,next) {
     passport.authenticate('twitter',{
         callbackURL:"/auth/twitter/claim/callback/" + req.params.id,
         successRedirect:"/claim/" + req.params.id,
