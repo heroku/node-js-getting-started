92607f3dd1df6a1e34a0c729f50fef9a588b15a7
Thibault Bedrignans <thib.bedrignans@gmail.com>
2017-11-13 16:43:33 +0100

    Change callback mongoose function to async function

--- app/src/routes/faces.js
+++ app/src/routes/faces.js
@@ -6,7 +6,7 @@ import config from '../config';
 
 const router = express.Router();
 
-router.post('/',(req, res) => {
+router.post('/',async (req, res) => {
   var face = new Face();      // create a new instance of the Face model
   face.accountname = req.body.accountname;  // set the faces name (comes from the request)
   face.firstname = req.body.firstname;  // set the faces name (comes from the request)
@@ -17,122 +17,102 @@ router.post('/',(req, res) => {
   face.network = req.body.network;  // set the faces name (comes from the request)
 
   // save the face and check for errors
-  face.save((err) => {
-      if (err)
-          return res.send(err);
-      res.json({ message: 'Face created!' });
-  });
+  try{
+    let response = await face.save();
+    res.json({message : 'Face created'});
+  }catch(err){
+    res.json(err);
+  }
 });
 
 // get all the faces (accessed at GET http://localhost:8080/api/faces)
-router.get('/',(req, res) => {
-    Face.find((err, faces) => {
-        if (err)
-            return res.send(err);
-        res.json(faces);
-    });
+router.get('/', async (req, res) => {
+  try{
+    res.json(await Face.find());
+  }catch(err){
+    res.json(err);
+  }
 });
 
-router.get('/:face_id', (req, res) => {
-    Face.findById(req.params.face_id, (err, face) => {
-        if (err)
-            return res.send(err);
-        res.json(face);
-    });
+router.get('/:id', async (req, res) => {
+  try{
+    res.json(await Face.findById(req.params.id));
+  }catch(err){
+    res.json(err);
+  }
 });
 
-router.post('/:face_id', (req, res) =>{
-    Face.findById(req.params.face_id, (err, face) => {
-        if (err)
-          return res.send(err);
-        face.occupations = JSON.stringify(req.body.occupations);
-        face.lang = req.body.lang;
-        face.website = req.body.website;
-        face.accountname = req.body.accountname;
-
-        face.save((err) => {
-            if (err)
-                return res.send(err);
-            res.json({ 'message': 'Face saved!', 'face': face});
-        });
-    });
+router.post('/:id', async (req, res) => {
+  try{
+    let data_update =  {occupation, lang, website, accountname} = req.body;
+    res.json(await Face.findOneAndUpdate(req.params.id, data_update, {new: true}));
+  }catch(err){
+    res.json(err);
+  }
 });
 
-router.delete('/:face_id', (req, res) => {
-    Face.remove({
-        _id: req.params.face_id
-    }, (err, face) => {
-        if (err)
-            return res.send(err);
-        res.json({ message: 'Successfully deleted' });
-    });
+router.delete('/:id', async (req, res) => {
+  try{
+    await Face.remove({_id : req.params.id});
+    res.json({message: 'Successfully deleted'});
+  }catch(err){
+    res.json(err);
+  }
 });
 
-router.get('/not_human/:number', (req, res) => {
-    Face.findOneAndUpdate({"number": req.params.number}, {not_human : true}, {new : true}, (err, face) => {
-        if (err)
-          return res.send(err);
-        res.json({ 'message': 'Face Not Human saved!', 'face': face});
-    });
+router.get('/not_human/:number', async (req, res) => {
+  try{
+    let face = await Face.findOneAndUpdate({"number": req.params.number}, {not_human : true}, {new : true});
+    res.json({ 'message': 'Face Not Human saved!', 'face': face});
+  }catch(err){
+    res.json(err);
+  }
 });
 
-router.get('/number/:number', (req,res) => {
-    var number = req.params.number;
-    Face.find({number:{$gt:(number - 1),$lt:(number + config.faces_by_request)}}).sort('number').limit(config.faces_by_request).exec((err, faces) =>{
-          var tempFaces = _.clone(faces);
-
-          if (err)
-            return res.send(err);
-          for(var i = number; i < parseInt(number, 10) + parseInt(config.faces_by_request, 10); i++){
-            if( ! _.find(tempFaces, (currentFace) => { return currentFace.number == i; }) ){
-              tempFaces.push({'number': i});
-            }
-          }
-        tempFaces = _.sortBy(tempFaces, 'number');
-        res.json(tempFaces);
-    });
-});
-
-router.get('/range/:range', (req, res) => {
-    var range = JSON.parse('[' + req.params.range + ']');
-
-    Face.find({number:{$in:range}}).sort('number').exec((err, faces) => {
-
-        var tempFaces = _.clone(faces);
-        if (err)
-            return res.send(err);
-        for(var i = 0; i < range.length; i++){
-
-            if( ! _.find(tempFaces, (currentFace) => { return currentFace.number == range[i]; }) ){
-                tempFaces.push({'number': range[i]});
-            }
-        }
-
-        tempFaces = _.sortBy(tempFaces, 'number');
-        res.json(tempFaces);
-    });
-});
-
-
-router.get('/search/:query', (req, res) => {
-    var regex = new RegExp('.*' + req.params.query + '.*', "i");
-    var testInt = parseInt(req.params.query, 10);
-
-    if(_.isNaN(testInt)){
-        Face.find({accountname: regex}).limit(config.faces_by_search).exec((err, faces) => {
-            //console.log('SEARCH BY ACCOUNT NAME', faces);
-            if (err)
-                return res.send(err);
-            res.json(faces);
-        });
-    }else{
-        Face.find({number: req.params.query}).limit(config.faces_by_search).exec((err, faces) => {
-            //console.log('SEARCH BY NUMBER', faces);
-            if (err)
-                return res.send(err);
-            res.json(faces);
-        });
+router.get('/number/:number', async (req,res) => {
+  try{
+    let number = req.params.number;
+    let faces = await Face.find({number:{$gt:(number - 1),$lt:(number + config.faces_by_request)}}).sort('number').limit(config.faces_by_request).exec();
+    let tempFaces = _.clone(faces);
+    for(var i = number; i < parseInt(number, 10) + parseInt(config.faces_by_request, 10); i++){
+      if( ! _.find(tempFaces, (currentFace) => { return currentFace.number == i; }) ){
+        tempFaces.push({'number': i});
+      }
     }
+    tempFaces = _.sortBy(tempFaces, 'number');
+    res.json(tempFaces);
+  }catch(err){
+    res.json(err);
+  }
+});
+
+router.get('/range/:range', async (req, res) => {
+  try{
+    let range = JSON.parse('[' + req.params.range + ']');
+    let faces = await Face.find({number: {$in: range}}).sort('number').exec();
+    let tempFaces = _.clone(faces);
+    for(var i = 0; i < range.length; i++){
+      if( ! _.find(tempFaces, (currentFace) => { return currentFace.number == range[i]; }) )
+        tempFaces.push({'number': range[i]});
+    }
+    tempFaces = _.sortBy(tempFaces, 'number');
+    res.json(tempFaces);
+  }catch(err){
+    res.json(err);
+  }
+});
+
+
+router.get('/search/:query', async (req, res) => {
+  try{
+    let regex = new RegExp('.*' + req.params.query + '.*', "i");
+    let testInt = parseInt(req.params.query, 10);
+    var query = _.isNaN(testInt) ? {accountname: regex} : {number: req.params.query};
+    let faces = await Face.find(query).limit(config.faces_by_search).exec();
+    res.json(faces);
+  }catch(err){
+    res.json(err);
+  }
 });
 
 export default router;
