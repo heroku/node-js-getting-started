9e380e6073dff7a872de1ac8860c380d288c1028
Thibault Bedrignans <thib.bedrignans@gmail.com>
2017-11-13 17:03:54 +0100

    Unify indent

--- app/src/routes/login.js
+++ app/src/routes/login.js
@@ -8,30 +8,27 @@ router.get('/facebook', (req,res,next) => {
     passport.authenticate('facebook',{callbackURL: '/login/facebook/callback' })(req,res,next);
 });
 
-router.get('/facebook/callback', passport.authenticate('facebook', {callbackURL: '/login/facebook/callback', failureRedirect: '/#login' }),
-  (req, res) => {
-    // Successful authentication, redirect home.
-
-    if(req.user.number){
-      res.redirect('/#number/' + req.user.number);
-    }else{
-      res.redirect('/#nonumber');
-    }
-
+router.get('/facebook/callback', passport.authenticate('facebook', {callbackURL: '/login/facebook/callback', failureRedirect: '/#login' }), (req, res) => {
+  // Successful authentication, redirect home.
+  if(req.user.number){
+    res.redirect('/#number/' + req.user.number);
+  }else{
+    res.redirect('/#nonumber');
+  }
 });
 
 router.get('/twitter',(req,res,next) => {
-    passport.authenticate('twitter',{callbackURL: '/login/twitter/callback' })(req,res,next);
+  passport.authenticate('twitter',{callbackURL: '/login/twitter/callback' })(req,res,next);
 });
 
 router.get('/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/#login' }),(req, res) => {
-        // Successful authentication, redirect home.
-    res.redirect('/#number/' + req.user.number);
+  // Successful authentication, redirect home.
+  res.redirect('/#number/' + req.user.number);
 });
 
 router.get('/logout', (req, res) => {
-    req.logout();
-    res.redirect('/');
+  req.logout();
+  res.redirect('/');
 });
 
 export default router;
