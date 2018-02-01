4556a5848986d6649e2e18d215161badd83d8038
Brigitte Schuster <5750799+bschuste@users.noreply.github.com>
2018-01-31 21:08:24 -0500

    Added integers for month and day

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -13,12 +13,14 @@ function usersPut(req, res) {
   var id = req.swagger.params.userId.value;
   var firstName = req.swagger.params.userInfo.firstName || 'John';
   var lastName = req.swagger.params.userInfo.lastName || 'Doe';
-  var birthDate = req.swagger.params.userInfo.birthDate || 1970;
+  var birthMonth = req.swagger.params.userInfo.birthMonth || 1;
+  var birthDay = req.swagger.params.userInfo.xbirthDay || 1;
+  var birthYear = req.swagger.params.userInfo.birthYear || 1970;
   var email = req.swagger.params.userInfo.email || 'john.doe@users.com';
   var response = util.format('User ID:%d,', id);
   response += util.format('firstName:%s,', firstName);
   response += util.format('lastName:%s,', lastName);
-  response += util.format('birthDate:%d,', birthDate);
+  response += util.format('birthDate:%d-%d-%d,', birthMonth, birthDay, birthYear);
   response += util.format('email:%s', email);
 
   res.json({'message': response});
