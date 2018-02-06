8b13978f444f0e38726894725f76e57c28307711
Bret Walker <bw@bretw.com>
2018-02-05 20:32:03 -0500

    Getting closer

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -10,28 +10,39 @@ module.exports = {
 function usersPut(req, res) {
   // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
   var id = req.swagger.params.userId.value;
-  console.log(req.swagger.params.userInfo);
-
-  var firstName = req.swagger.params.userInfo.firstName;
-  var lastName = req.swagger.params.userInfo.lastName;
-  var birthMonth = req.swagger.params.userInfo.birthMonth;
-  var birthDay = req.swagger.params.userInfo.birthDay;
-  var birthYear = req.swagger.params.userInfo.birthYear;
-  var birthDate = util.format('%d-%d-%d,', birthYear, birthMonth, birthDay);
-  var email = req.swagger.params.userInfo.email;
-
+  var firstName = req.swagger.params.userInfo.schema.firstName;
+  var lastName = req.swagger.params.userInfo.schema.lastName;
+  var birthMonth = req.swagger.params.userInfo.schema.birthMonth;
+  var birthDay = req.swagger.params.userInfo.schema.birthDay;
+  var birthYear = req.swagger.params.userInfo.schema.birthYear;
+  var birthDate = util.format('%d-%02d-%02d,', birthYear, birthMonth, birthDay);
+  var email = req.swagger.params.userInfo.schema.email;
 
   db.users.findById(id)
     .then(user => {
-      user.firstName = firstName;
-      user.lastName = lastName;
-      user.email = email;
-      user.birthDate = birthDate;
+      if (user) {
+        user.update({
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          //birthDate: Date.parse(birthDate)
+        })
+        .then(() => {
+          
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).send({'message': 'ERROR'})
+        });
+      } else {
+        res.status(400).send({'message': 'User not found'})
+      }
+
       user.save()
-      res.json(200).json({'message': 'OK'})
+      res.json({'message': 'OK'})
     })
     .catch(err => {
       console.log(err);
-      res.json(500).json({'message': 'ERROR'})
+      res.status(500).send({'message': 'ERROR'})
     });
 }
