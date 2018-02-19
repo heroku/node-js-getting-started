3230e7b8d54360b6c3955d68afe45a78cbd10d45
Brigitte Schuster <5750799+bschuste@users.noreply.github.com>
2018-02-19 10:27:13 -0500

    Initial implementation of authentication api

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -4,7 +4,9 @@ var util = require('util');
 var db = require('../db')
 
 module.exports = {
-  usersPut: usersPut
+  usersPut: usersPut,
+  usersLogin: usersLogin,
+  usersLogout: usersLogout
 };
 
 function usersPut(req, res) {
@@ -18,7 +20,6 @@ function usersPut(req, res) {
   var email = req.swagger.params.userInfo.value.email;
 
   if ((birthDay>0 && birthDay<32) && (birthMonth>0 && birthMonth<13) && birthYear) {
-
     db.users.findById(id)
       .then(user => {
         if (user) {
@@ -42,4 +43,15 @@ function usersPut(req, res) {
   } else {
     res.json({'message': 'Wrong date'}, 400);
   }
+}
+
+function usersLogin(req, res) {
+  var email    = req.swagger.params.userCredentials.value.email;
+  var password = req.swagger.params.userCredentials.value.password;
+  console.log("Username: " + email);
+  console.log("Password: " + password);
+  res.json({'token': 'OK'}, 200);
+}
+
+function usersLogout(req, res) {
 }
