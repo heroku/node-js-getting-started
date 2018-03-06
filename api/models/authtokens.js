62f0262afade6c319c7580a0f254b325e4b7518d
Matt Peyton <matthewcpeyton@gmail.com>
2018-03-05 19:11:05 -0500

    bug fix in model

--- api/models/authtokens.js
+++ api/models/authtokens.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-model.exports = function(sequelize, DataTypes){
+module.exports = function(sequelize, DataTypes){
   return sequelize.define('authtokens', {
     token: { type: Sequelize.STRING},
     expiration: { type: Sequelize.DATE},
