211cccf33055994c2b0bc7634608c783541a2fa7
Victor Montoya <victoramontoya@gmail.com>
2018-07-16 20:53:21 -0700

    deleted app, changed node engine

--- index.js
+++ index.js
@@ -3,8 +3,5 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.json({hello: 'hello world!'}))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
