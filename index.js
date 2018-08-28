3259f3c9033369324a483edc9e1be4b9675c3b1e
Yuriy Gavrylenko <ygavrylenko@ygavrylenko-ltm.internal.salesforce.com>
2018-08-28 22:32:30 +0200

    Cool faces added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const cool = require('cool-ascii-faces')
 const PORT = process.env.PORT || 5000
 
 express()
@@ -7,4 +8,5 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
