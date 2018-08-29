2ce2d93e9b22b29350a26ad6d826b56a6d41f710
Yuriy Gavrylenko <ygavrylenko@ygavrylenko-ltm.internal.salesforce.com>
2018-08-29 16:33:19 +0200

    Change to prefix salesforce

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ express()
   .get('/db', async (req, res) => {
       try {
         const client = await pool.connect()
-        const result = await client.query('SELECT * FROM property__c');
+        const result = await client.query('SELECT * FROM salesforce.property__c');
         res.render('pages/db', result);
         client.release();
       } catch (err) {
