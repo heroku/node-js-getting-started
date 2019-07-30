40a4d50194414504b6b0def13ae342406f28138e
Siva Kumar <42864093+usirikayalasiva@users.noreply.github.com>
2019-07-30 12:18:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -7,4 +8,5 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
