a6eb429fe73a7364d2c62d47a0c43c5b80234363
Daniel Marashlian <danielzev@gmail.com>
2020-03-14 21:36:58 -0700

    add times

--- index.js
+++ index.js
@@ -9,4 +9,14 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
+  .get('/times', (req, res) => res.send(showTimes()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+showTimes = () => {
+  let result = ''
+  const times = process.env.TIMES || 5
+  for (i = 0; i < times; i++) {
+    result += i + ' '
+  }
+  return result;
+}
