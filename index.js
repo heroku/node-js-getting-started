d3ad34d1703b14f4ff8a6bdd4dabeecd1d581af2
Hugo Voisin <hugov@bam.tech>
2020-04-30 01:25:47 +0200

    feat: add get and post for polls

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require('express')
 const path = require('path')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 5000
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+let answers = { 11: 0, 12: 0 };
+
+const app = express();
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.get('/poll', cors(corsOptions), (req, res) => res.set({
+    'Content-Type': 'application/json'
+  }).json({
+    title: "Sondage",
+    answers: [
+      { id: "11", title: "La Guerre des étoiles" },
+      { id: "12", title: "Le Monde de Nemo" }]
+  })
+);
+
+app.post(
+  '/poll/vote',
+  cors(corsOptions),
+  (req, res) => {
+    const answerId = req.body.answerId;    
+    answers = { ...answers, [answerId]: answers[answerId] + 1 };
+    res.json(answers);
+  }
+);
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
