bf2c254e52b473c2fc3100592c64cf7b5e0d4100
sonalisahu64 <63224427+sonalisahu64@users.noreply.github.com>
2020-05-22 17:28:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -8,3 +27,5 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
