e15e299e2ada4978a7b59b82ffda790a525e40ee
joshuscurtis <joshuscurtis@gmail.com>
2020-07-10 16:49:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,13 +23,7 @@ app.use(function (req, res, next) {
     next();
 });
 
-const request = require('request');
 
-request('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); }
-  console.log(body.url);
-  console.log(body.explanation);
-});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
