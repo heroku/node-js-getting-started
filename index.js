73018d9a1aab17f14b0c649c74c14dc835f2b983
joshuscurtis <joshuscurtis@gmail.com>
2020-07-10 16:34:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,40 +2,6 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 var app = express();
-    var request = require('request');
-
-// Add headers
-app.use(function (req, res, next) {
-// Website you wish to allow to connect
-res.setHeader('Access-Control-Allow-Origin', '*');
-// Request methods you wish to allow
-res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-// Request headers you wish to allow
-res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-// Set to true if you need the website to include cookies in the requests sent
-// to the API (e.g. in case you use sessions)
-res.setHeader('Access-Control-Allow-Credentials', true);
-// Pass to next layer of middleware
-next();
-});
-const request = require('request');
-request('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', { json: true }, (err, res, body) => {
-if (err) { return console.log(err); }
-console.log(body.url);
-console.log(body.explanation);
-});
-express()
-.use(express.static(path.join(__dirname, 'public')))
-.set('views', path.join(__dirname, 'views'))
-.set('view engine', 'ejs')
-.get('/', (req, res) => res.render('pages/index'))
-.get('/table', (req, res) => res.render('pages/table'))
-.listen(PORT, () => console.log(`Listening on ${ PORT }`))
-.get('/data', function(req, res){
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-var app = express();
 
 // Add headers
 app.use(function (req, res, next) {
@@ -92,5 +58,3 @@ https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
 });
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-})
-.listen(PORT, () => console.log(`Listening on ${ PORT }`))
