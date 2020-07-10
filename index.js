9d390f7fd49dc0ab36d0e4eddfb2dfb2dce9aceb
joshuscurtis <joshuscurtis@gmail.com>
2020-07-10 16:46:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ var req = https.request(options, function (res) {
 
   res.on("end", function (chunk) {
     var body = Buffer.concat(chunks);
-    console(body.toString());
+    console.log(body.toString());
   });
 
   res.on("error", function (error) {
