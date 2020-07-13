57233fec1f020aa8e8de6a33e4bbb56ab2626068
joshuscurtis <joshuscurtis@gmail.com>
2020-07-13 12:18:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -147,32 +147,6 @@ setInterval(function() {
 
 
 
-
-// Socket.io server listens to our app
-var server = http.createServer(app);
-var io = require('socket.io').listen(server);  //pass a http.Server instance
-server.listen(80);
-
-
-// Send current time to all connected clients
-function sendTime() {
-    io.emit('time', { time: new Date().toJSON() });
-}
-
-// Send current time every 10 secs
-setInterval(sendTime, 10000);
-
-// Emit welcome message on connection
-io.on('connection', function(socket) {
-    // Use socket to communicate with this particular client only, sending it it's own id
-    socket.emit('welcome', { message: 'Welcome!', id: socket.id });
-
-    socket.on('i am client', console.log);
-});
-
-
-
-
 express()
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
