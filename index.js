06fb5a3977ec95bb43aac58bf02d90e492653061
joshuscurtis <joshuscurtis@gmail.com>
2020-07-13 12:36:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -150,7 +150,19 @@ setInterval(function() {
     });
 }, 1000)
 
+io.on('connection', function(client) {
+    console.log('Client connected...');
 
+    client.on('join', function(data) {
+        console.log(data);
+    });
+
+    client.on('messages', function(data) {
+           client.emit('broad', data);
+           client.broadcast.emit('broad',data);
+    });
+
+});
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
