c7aed83dafb975f62929a3f87c4eb58572080867
joshuscurtis <joshuscurtis@gmail.com>
2020-08-05 17:23:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require('express')
 const path = require('path')	
 const PORT = process.env.PORT || 5000	
-var app = express()
-var http = require('http')
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
+app.get('/', function(req, res) {
+   res.sendfile('index.html');
+});
 
+//Whenever someone connects this gets executed
+io.on('connection', function(socket) {
+   console.log('A user connected');
 
+   //Whenever someone disconnects this piece of code executed
+   socket.on('disconnect', function () {
+      console.log('A user disconnected');
+   });
+});
 
-
+http.listen(5000, function() {
+   console.log('listening on *:5000');
+});
 
 
 // Add headers
