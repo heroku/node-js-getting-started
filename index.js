eb686d73d0ca275139ac6e32617d90b8d9879cb8
Josh <joshuscurtis@gmail.com>
2020-08-20 17:45:32 +0100

    socket

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ const basicAuth = require('express-basic-auth')
 
 
 
+const server = require('http').createServer(app);
+const options = { /* ... */ };
+const io = require('socket.io')(server, options);
+
+io.on('connection', socket => { /* ... */ });
+
+//server.listen( );
+
+
+
+
 
 
 // Add headers
