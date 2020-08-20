a23a6e310c3115fd8ebce715382b78f862322450
Josh <joshuscurtis@gmail.com>
2020-08-20 22:47:54 +0100

    pg

--- index.js
+++ index.js
@@ -26,7 +26,30 @@ io.on('connection', function(client) {
 
 //pg
 
-
+const { Pool, Client } = require('pg')
+const pool = new Pool({
+  user: 'dbuser',
+  host: 'database.server.com',
+  database: 'mydb',
+  password: 'secretpassword',
+  port: 3211,
+})
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
+const client = new Client({
+  user: 'josh',
+  host: 'theway.c15j82hx0pnm.us-east-2.rds.amazonaws.com',
+  database: 'postgres',
+  password: 'Rufus912',
+  port: 5432,
+})
+client.connect()
+client.query('SELECT * FROM public.orders', (err, res) => {
+  console.log(err, res)
+  client.end()
+})
 
 
 
