3f7583203ebbf26d868f637a898de923c357c2e0
Josh <joshuscurtis@gmail.com>
2020-08-23 16:01:52 +0100

    no hours

--- public/script.js
+++ public/script.js
@@ -406,7 +406,7 @@ function createTime(unixdate) {
 	// Seconds part from the timestamp
 	var seconds = "0" + date.getSeconds();
 	// Will display time in 10:30:23 format
-	var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+	var formattedTime = minutes.substr(-2) + ':' + seconds.substr(-2);
 	
 	return formattedTime;
 }
