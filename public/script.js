9eabd414f943af7dc85695d5388c9488737d962d
Josh <joshuscurtis@gmail.com>
2020-08-23 16:01:02 +0100

    slahighlight

--- public/script.js
+++ public/script.js
@@ -203,6 +203,7 @@ function drawNth(x, table) {
 		g.setAttribute("onclick", 'highlight(this);')
 			//highlight for processing 
 		if(isProcessing(divId)) highlight2(g)
+		SLAHighlight(divId);
 	}
 
 }
