ab512ce923f69eea79c70f8d9ddeb14842c8763d
Josh <joshuscurtis@gmail.com>
2020-08-23 22:47:09 +0100

    react

--- public/react.js
+++ public/react.js
@@ -1,13 +1,24 @@
-'use strict'
+'use strict';
 
-const LittleApp = () => {
-  return (
-    <div>
-      <MaterialUI.Button variant="contained" color="primary">
-        Hello World
-      </MaterialUI.Button>
-    </div>
-  )
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
 }
 
-ReactDOM.render(<LittleApp />, document.getElementById("like_button_container"));
+ReactDOM.render(e(LikeButton), document.getElementById("like_button_container"));
