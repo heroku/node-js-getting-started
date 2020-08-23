7d6702100d04be81081dcd8fae74984c82753aba
Josh <joshuscurtis@gmail.com>
2020-08-23 22:36:40 +0100

    react

--- public/react.js
+++ public/react.js
@@ -1,17 +1,11 @@
 const LittleApp = () => {
   return (
-    <AppBar position="static">
-  <Toolbar>
-    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-      <MenuIcon />
-    </IconButton>
-    <Typography variant="h6" className={classes.title}>
-      News
-    </Typography>
-    <Button color="inherit">Login</Button>
-  </Toolbar>
-  </AppBar>
-  );
-};
+    <div>
+      <MaterialUI.Button variant="contained" color="primary">
+        Hello World
+      </MaterialUI.Button>
+    </div>
+  )
+}
 
 ReactDOM.render(<LittleApp />, document.getElementById("like_button_container"));
