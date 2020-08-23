b1fcda74094ce46c1572ad02c39941896a0fd555
jecarr <jenine_c@outlook.com>
2020-08-23 12:17:40 +1200

    Retrieved all toilet data for Palmerston North

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const toiletURL = 'https://geosite.pncc.govt.nz/arcgis/rest/services/Public/WEB_Parks/MapServer/1/query?where=1%3D1&outFields=*&outSR=4326&f=json'
+const request = require('request')
+
+let toiletData = {}
+// Get the toilet data 
+request(toiletURL, { json: true }, (err, resp, body) => {
+  if (err) { return console.log(err) }
+  
+  toiletData = body.features
+  console.log(toiletData)
+});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
