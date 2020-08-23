5e0c3c51d3c9cb1974a6817c41a10618f0021436
Josh <joshuscurtis@gmail.com>
2020-08-23 21:54:42 +0100

    react  fix

--- public/react.js
+++ public/react.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
+import {React} from 'react';
+import {ReactDOM} from 'react-dom';
+import {Button} from '@material-ui/core/Button';
 
 function App() {
   return (
