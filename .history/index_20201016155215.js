45f07f8fa1efc1eb887dfdf55e867a208d2b7889
Abhishek Srivastava (CRM) - QIAGEN <Abhi.Srivastava@qiagen.com>
2020-10-16 15:55:42 +0200

    Add cool face API

--- .history/index_20201016155215.js
+++ .history/index_20201016155215.js
@@ -1,4 +1,3 @@
-const cool = require('cool-ascii-faces');
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
@@ -8,5 +7,4 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
