1ff48039b4b28a83255aa4aa951ced7b92f8d489
Jérémy Brunet <63155312+Jeremy-AM@users.noreply.github.com>
2021-01-05 23:26:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const PORT = process.env.PORT || 5000
 var BasecampStrategy = require('passport-basecamp').Strategy;
 
 passport.use(new BasecampStrategy({
-    clientID: a744476797ca7342f8824b03138d07c47cfba4ad,
-    clientSecret: 8a3554eb0b3db98502eded261f69ff8898795690,
+    clientID: "a744476797ca7342f8824b03138d07c47cfba4ad",
+    clientSecret: "8a3554eb0b3db98502eded261f69ff8898795690",
     callbackURL: "https://obscure-taiga-20645.herokuapp.com/auth/basecamp/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
