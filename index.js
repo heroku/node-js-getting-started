f999513dc779f57d651a9bfa3458b578e71bc6bc
Jacky <winterzaki@gmail.com>
2021-01-28 14:47:23 +0900

    Engaged Express

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express=require('express');
+const app=express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const port=process.env.PORT || 8080;
+app.get('/', (req, res)=>{ res.send('Hello, World'); });
+app.listen(port, ()=>{ console.log(`Express Server Listen START at port=${port}`); });
