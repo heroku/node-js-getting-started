83f2120dcaf964922dc997c2d043e3bfc02950a4
Yogesh <yogimayi@outlook.com>
2021-02-09 18:23:49 +0530

    make it better

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+var app = require('express')();
+var path = require('path');
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var fs = require('fs');
+app.use(express.static(__dirname));
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/', (req, res) => {  
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('yogi', (msg) => {
+    console.log('message: ' + msg);
+    fs.readFile('./public/node.svg', (err, buff) => {
+      socket.emit('reply', buff);
+    })    
+  });  
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
