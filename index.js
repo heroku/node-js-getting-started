999099f60b363191c68e5bdf66ee8bca2f7a118a
Thanapat <thanapat.m.sk131@gmail.com>
2021-02-19 10:32:33 +0700

    set default html

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+ .use(express.static(path.join(__dirname, "public")))
+ .set("views", path.join(__dirname, "views"))
+ .get("/", (req, res) => res.render("pages/index"))
+ .listen(PORT, () => console.log(`Listening on ${PORT}`));
