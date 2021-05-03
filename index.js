4662473707353d39a2ebe7bd9393c261c9f0603c
Dave Foley <34948351+df2112@users.noreply.github.com>
2021-05-03 15:11:35 -0400

    WIP summary report done

--- index.js
+++ index.js
@@ -5,15 +5,18 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 
-const CLIENT_ID = "3MVG9cHH2bfKACZaxOl_SD58KkCHXUgFnvVjQlpp3mrVQ.uVow6vx_oCG3SFG8wQ6OCdc7uGthgSS9RPSkrRL";
-const CLIENT_SECRET = "2B6980F88F562E5B8014CF1587DEAA6D82B4E388016BB9019324F4C61B3ECDC1";
+const CLIENT_ID =
+  "3MVG9cHH2bfKACZaxOl_SD58KkCHXUgFnvVjQlpp3mrVQ.uVow6vx_oCG3SFG8wQ6OCdc7uGthgSS9RPSkrRL";
+const CLIENT_SECRET =
+  "2B6980F88F562E5B8014CF1587DEAA6D82B4E388016BB9019324F4C61B3ECDC1";
 const USER_NAME = "dfoley@dfoley-21-spring.demo";
 const PASSWORD = "Sandbox2101!";
 
 const OAUTH_PATH = "/services/oauth2/token?grant_type=password";
 const OAUTH_URL = `https://login.salesforce.com${OAUTH_PATH}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&username=${USER_NAME}&password=${PASSWORD}`;
 
-const FLOW_API_URL = "https://dfoley-21-spring-demo.my.salesforce.com/services/data/v50.0/actions/custom/flow/Headunit_Flow_API";
+const FLOW_API_URL =
+  "https://dfoley-21-spring-demo.my.salesforce.com/services/data/v50.0/actions/custom/flow/Headunit_Flow_API";
 
 express()
   .use(express.static(path.join(__dirname, "public")))
@@ -34,11 +37,71 @@ async function daveTest(req, res) {
   let responseJSON;
 
   switch (actionId) {
-    case 'notifications':
-    case 'offers':
-    case 'options':
+    case "notifications":
+    case "options":
+    case "summary":
       bearerToken = await postOAuth();
       responseJSON = await postHeadUnitAPI(bearerToken, actionId, itemId, VIN);
+      console.log(
+        "ENTRY => Handled actionId/itemsId\n".green,
+        `${actionId}/${itemId}`
+      );
+      break;
+    case "offers":
+      switch (itemId) {
+        case "dataplan":
+          responseJSON = getOffersDataPlan();
+          console.log(
+            "ENTRY => Faked actionId/itemsId\n".cyan,
+            `${actionId}/${itemId}`
+          );
+          break;
+        case "application":
+          responseJSON = getOffersApplication();
+          console.log(
+            "ENTRY => Faked actionId/itemsId\n".cyan,
+            `${actionId}/${itemId}`
+          );
+          break;
+      }
+      break;
+    case "buy":
+      responseJSON = getBuy(itemId);
+      console.log(
+        "ENTRY => Faked actionId/itemsId\n".cyan,
+        `${actionId}/${itemId}`
+      );
+      break;
+    case "gas":
+      responseJSON = getGasNavigate();
+      console.log(
+        "ENTRY => Faked actionId/itemsId\n".cyan,
+        `${actionId}/${itemId}`
+      );
+      break;
+    case "roadside":
+      responseJSON = getRoadsideCase();
+      console.log(
+        "ENTRY => Faked actionId/itemsId\n".cyan,
+        `${actionId}/${itemId}`
+      );
+      break;
+    case "tires":
+      responseJSON = getTiresShop();
+      console.log(
+        "ENTRY => Faked actionId/itemsId\n".cyan,
+        `${actionId}/${itemId}`
+      );
+      break;
+    // case 'summary':
+    //   responseJSON = getSummaryReport();
+    //   console.log('ENTRY => Faked actionId/itemsId\n'.cyan, `${actionId}/${itemId}`);
+    //   break;
+    default:
+      console.log(
+        "ENTRY => Unhandled actionId/itemsId\n".red,
+        `${actionId}/${itemId}`
+      );
       break;
   }
 
@@ -55,10 +118,10 @@ async function postOAuth() {
   };
 
   await axios(axiosConfig)
-    .then((res) => bearerToken = "Bearer " + res.data.access_token)
+    .then((res) => (bearerToken = "Bearer " + res.data.access_token))
     .catch((error) => console.log(`OAuth Error: ${error}`));
 
-  console.log('EXIT postOAuth() => bearerToken\n'.green, bearerToken)
+  //console.log('EXIT postOAuth() => bearerToken\n'.green, bearerToken)
   return bearerToken;
 }
 
@@ -73,44 +136,119 @@ async function postHeadUnitAPI(bearerToken, actionId, itemId, VIN) {
   };
 
   await axios(axiosConfig)
-    .then((res) => {
-      flowResponse = mapFlowResponse(actionId, res);
-    })
+    .then((res) => flowResponse = mapFlowResponse(actionId, itemId, res))
     .catch((error) => console.log(`Flow Error: ${error}`));
 
-  console.log('EXIT postHeadUnitAPI() => flowResponse\n'.cyan, flowResponse)
+  //console.log('EXIT postHeadUnitAPI() => flowResponse\n'.cyan, flowResponse)
   return flowResponse;
 }
 
-function mapFlowResponse(actionId, res) {
+function mapFlowResponse(actionId, itemId, res) {
+  let coreOutputValues = res.data[0].outputValues;
   let coreResponse;
 
   switch (actionId) {
-    case 'notifications':
-      coreResponse = res.data[0].outputValues.DaveHeadUnitNotifications;
+    case "notifications":
+      coreResponse = coreOutputValues.DaveHeadUnitNotifications;
       break;
-    case 'offers':
-      coreResponse = res.data[0].outputValues.DaveHeadUnitOffers;
+    case "offers":
+      coreResponse = coreOutputValues.DaveHeadUnitOffers;
       break;
-    case 'options':
-      coreResponse = res.data[0].outputValues.DaveHeadUnitOptions;
+    case "options":
+      coreResponse = coreOutputValues.DaveHeadUnitOptions;
+      break;
+    case "summary":
+      return coreOutputValues.HeadUnitReportSummaryResponse;
       break;
   }
 
   let flowResponseDave = {
-    [actionId]: coreResponse.map(src => {
+    [actionId]: coreResponse.map((src) => {
       return {
-        title: 'Dave71 ' + src.ShortDescription__c,
-        itemId: src.producttype__c ? src.producttype__c : '',
+        title: "Dave71 " + src.ShortDescription__c,
+        itemId: src.producttype__c ? src.producttype__c : "",
         actionId: src.Type__c,
-        shortDescription: 'Dave 72 ' + src.ShortDescription__c,
+        shortDescription: "Dave 72 " + src.ShortDescription__c,
         longDescription: src.LongDescription__c,
         imageurl: src.imageurl__c,
-        price: '',
-        buttons: src.button__c ? src.button__c : ''
+        price: "",
+        buttons: src.button__c ? src.button__c : "",
       };
-    })
+    }),
   };
 
   return flowResponseDave;
 }
+
+function getOffersDataPlan() {
+  return {
+    offers: [
+      {
+        title: "Reserve a Parking Spot",
+        itemId: "DP-100",
+        actionId: "buy",
+        shortDescription: "Reserve a Parking Spot",
+        longDescription: "You look very tired. Reserve a Parking Spot",
+        imageurl: "https://nissantosf.herokuapp.com/level3.png",
+        price: "25.00",
+        buttons: "Reserve",
+      },
+      {
+        title: "Reserve a Shower",
+        itemId: "DP-102",
+        actionId: "buy",
+        shortDescription: "You can reserve a shower here.",
+        longDescription: "You can reserve a shower here.",
+        imageurl: "https://nissantosf.herokuapp.com/level2.png",
+        price: "10.00",
+        buttons: "Reserve",
+      },
+      {
+        title: "Order at Subway",
+        itemId: "DP-104",
+        actionId: "buy",
+        shortDescription: "Order at Subway",
+        longDescription: "Our sandwiches are delicious.",
+        imageurl: "https://nissantosf.herokuapp.com/level1.png",
+        price: "5.00",
+        buttons: "Order",
+      },
+    ],
+  };
+}
+
+function getOffersApplication() {
+  return {
+    offers: [
+      {
+        title: "Hydrogen Fuel",
+        itemId: "AP-100",
+        actionId: "buy",
+        shortDescription: "Hydrogen Fuel",
+        longDescription: "Fill up now with totally Green Hydrogen Fuel!",
+        imageurl: "https://nissantosf.herokuapp.com/hydrogen.png",
+        price: "",
+        buttons: "Reserve",
+      },
+      {
+        title: "Wind Turbine EV Charging Station",
+        itemId: "AP-106",
+        actionId: "buy",
+        shortDescription: "Wind Turbine EV Charging Station",
+        longDescription:
+          "Reserve a spot at the Wind Turbine EV Charging Station",
+        imageurl: "https://nissantosf.herokuapp.com/Pumpkin.png",
+        price: "",
+        buttons: "Reserve",
+      },
+    ],
+  };
+}
+
+function getBuy(itemId) {}
+
+function getGasNavigate() {}
+
+function getRoadsideCase() {}
+
+function getTiresShop() {}
