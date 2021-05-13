df63c769640ae566f041ddce50fa3fa3a5947ec4
navidEns <navid.ens@gmail.com>
2021-05-13 17:23:59 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const cool = require('cool-ascii-faces');
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
