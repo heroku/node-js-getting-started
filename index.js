28b7a322ebc4f6a041cdebd42390bfa1e206f07e
AayushVER <85309344+AayushVER@users.noreply.github.com>
2021-06-11 17:24:15 +0530

    keep it simple

--- index.js
+++ index.js
@@ -7,4 +7,5 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(process.env.PORT || 5000
+, () => console.log(`Listening on ${ PORT }`))
