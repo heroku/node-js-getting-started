d1f522238270acc20d2d2a95429c9262a18eb70a
RoboDevelop <dhav05suthar@gmail.com>
2021-10-24 18:28:46 +0530

    launch

--- src/Call.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Call() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
--- src/Chat.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Chat() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
