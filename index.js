f498f7fe690557095322b3ee5807982fcf7c513b
Jorge Luis Sampaio de Oliveira <jorgeluis.geek@gmail.com>
2022-01-28 20:53:14 -0300

    update times

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+showTimes = () => {
+  let result = '';
+  const times = process.env.TIMES || 5;
+  for (i = 0; i < times; i++) {
+    result += i + ' ';
+  }
+  return result;
+}
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
+  .get('/times', (req, res) => res.send(showTimes()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
