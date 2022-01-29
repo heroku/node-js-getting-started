c927fb08c834ba79aa639b5548c4b6918701dc4e
Jorge Luis Sampaio de Oliveira <jorgeluis.geek@gmail.com>
2022-01-28 21:15:09 -0300

    add project postgresql

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const cool = require('cool-ascii-faces')
+
+const { Pool } = require('pg')
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -19,4 +28,16 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .get('/times', (req, res) => res.send(showTimes()))
+  .get('/db', async (req, res) => {
+    try{
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err){
+      console.error(err);
+      res.send("Error "+err)
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
