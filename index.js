eeb2249aa17f01cc224a7c4742976b4732a52143
Trey Mangat <tam14@sfu.ca>
2022-02-17 14:15:27 -0800

    initial push

--- index.js
+++ index.js
@@ -2,9 +2,50 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const { Pool } = require('pg');
+var pool;
+pool = new Pool({
+  connectionString: 'postgres://postgres:123456789@localhost/rectangle'
+})
+
+var app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('pages/index'));
+app.get('/database', (req,res) => {
+  var getRecsQuery = 'SELECT * FROM rectable';
+  pool.query(getRecsQuery, (error, result) => {
+    if(error)
+      res.end(error);
+    var results = {'rows':result.rows}
+    res.render('pages/db',results);
+  })
+});
+
+app.get('/results', (req,res) => {
+  var getNameQuery = 'SELECT * FROM rectable';
+  pool.query(getNameQuery, (error, result) => {
+    if(error)
+      res.end(error);
+    var Final = {'res':result.rows}
+    res.render('pages/result',Final);
+  })
+
+});
+// app.post('/adduser', (req,res)=>{
+//   var uname = req.body.uname;
+//   var age = req.body.age;
+//   res.send('username: ${uname}, age: ${age}');
+// });
+// app.get('/users/:id', (req,res)=>{
+//   var uid = req.params.id;
+//   console.log(req.params.id);
+//   //search the database using the uid
+//   res.send("got it!");
+// });
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
