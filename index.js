aac5dc2189df23da270d5cb88e553262f127a665
Credilar Central <entregascredlar@gmail.com>
2022-05-20 13:42:02 -0300

    teting db

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ express()
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect();
-      const result = await client.query('SELECT * FROM test_table');
+      const result = await client.query('SELECT * FROM produto');
       const results = { 'results': (result) ? result.rows : null};
       res.render('pages/db', results );
       client.release();
