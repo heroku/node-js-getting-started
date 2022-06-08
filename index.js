8d57fe4971c5f95e12814e0c7b39c3d77d8ca42e
Betsegaw Lemma Amersho <betsegawlemma@gmail.com>
2022-06-08 09:55:47 +0300

    update comment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 
-// express js app
+// express js app comment is updated
 express()
   .use(express.static(path.join(__dirname, "public")))
   .set("views", path.join(__dirname, "views"))
