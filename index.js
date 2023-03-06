5f25a7da5dc6400c19ddb14cf409404e31749ff4
Colin Casey <casey.colin@gmail.com>
2023-03-06 12:14:15 -0400

    Updates from freshness audit (#294)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+
+const PORT = process.env.PORT || 5001
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
