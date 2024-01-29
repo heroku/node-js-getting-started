8530355dee348cad566f9720759db108730ea14c
Ayaan Khan <ayaan.khan@salesforce.com>
2024-01-29 15:59:46 +0530

    update index js with MIAW

--- index.js
+++ index.js
@@ -9,3 +9,20 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  function initEmbeddedMessaging() {
+    try {
+        embeddedservice_bootstrap.settings.language = "en_US"; // For example, enter 'en' or 'en-US'
+
+        embeddedservice_bootstrap.init(
+            "00D7c000006tgG8",
+            "NW_edu_Deployment",
+            "https://formativ--dev.sandbox.my.site.com/ESWNWeduDeployment1705074359880",
+            {
+                scrt2URL: "https://formativ--dev.sandbox.my.salesforce-scrt.com",
+            }
+        );
+    } catch (err) {
+        console.error("Error loading Embedded Messaging: ", err);
+    }
+}
