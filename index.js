5b700915e59b52de91c462711af96cdf73f8a86e
Ayaan Khan <158028268+ayaan840645@users.noreply.github.com>
2024-01-29 18:30:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,21 +9,3 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-  function initEmbeddedMessaging() {
-    try {
-      console.log("here in index.js");
-      embeddedservice_bootstrap.settings.language = 'en_US'; // For example, enter 'en' or 'en-US'
-
-      embeddedservice_bootstrap.init(
-        '00D4C000000LPq8',
-        'NW_edu_Web_Deployment_English',
-        'https://formativ--devint.sandbox.my.site.com/ESWNWeduWebDeployment1706023343860',
-        {
-          scrt2URL: 'https://formativ--devint.sandbox.my.salesforce-scrt.com'
-        }
-      );
-    } catch (err) {
-      console.error('Error loading Embedded Messaging: ', err);
-    }
-  };
